1abf0530ebd6c2d6b6ef3cf1f6282daed9aabdb7
SamOP549 <jainsamarth549@gmail.com>
2023-02-08 00:05:05 +0530

    Config Fixed

--- next.config.js
+++ next.config.js
@@ -2,18 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
-  },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  images: {
-    unoptimized: true,
-  }
 };
 
 module.exports = nextConfig
